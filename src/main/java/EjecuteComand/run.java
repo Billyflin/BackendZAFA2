7ae22472db84d487c58607baa51b29fd222792ff
Billy <Billyflin>
2022-01-17 00:19:40 -0300

    a

--- src/main/java/EjecuteComand/run.java
+++ src/main/java/EjecuteComand/run.java
@@ -1,8 +1,29 @@
 package EjecuteComand;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class run {
-    public static void main(String[] args) {
-        execute e=new execute();
-        e.run();
+
+        public static void main(String[] args) throws Exception {
+            ProcessBuilder flask = new ProcessBuilder(
+                    "cmd.exe", "/c", "pip install Flask");
+            ProcessBuilder pywhatkit = new ProcessBuilder(
+                    "cmd.exe", "/c", "pip install pywhatkit");
+            ProcessBuilder builder = new ProcessBuilder(
+                    "cmd.exe", "/c", "Python src/main/java/service/util/main.py");
+            builder.redirectErrorStream(true);
+            flask.redirectErrorStream(true);
+            pywhatkit.redirectErrorStream(true);
+            Process p = builder.start();
+
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while (true) {
+                line = r.readLine();
+                if (line == null) { break; }
+                System.out.println(line);
+            }
+        }
     }
-}
+
