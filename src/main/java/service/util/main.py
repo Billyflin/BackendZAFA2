4de6af240139ad5cb4b2d9d8ae65dc1f19f52d3d
Billy <Billyflin>
2022-01-17 01:01:00 -0300

    e

--- src/main/java/service/util/main.py
+++ src/main/java/service/util/main.py
@@ -2,7 +2,7 @@ import pywhatkit
 
 try:
 
-    pywhatkit.sendwhatmsg_instantly("+56932633405", "Hola! tu código es 123456 :) ",5,True)
+    pywhatkit.sendwhatmsg_instantly("+56952307762", "Hola! tu código es 123456 :) ",5,True)
 
 except:
     print("error")
